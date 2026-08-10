1800adfc7fe8c7d734c83759e2ebdeb12509d41d
Zelle <mcijah14@gmail.com>
2026-08-10 10:21:01 +0800

    Initial commit with project structure and gitignore

---
+++ src/Student.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author mcija
+ */
+public class Student {
+    private String Name;
+    private String Id;
+    
+    public String getSN() {
+        return this.Name;
+    }
+    
+    public String getSID() {
+        return this.Id;
+    }
+}
+
